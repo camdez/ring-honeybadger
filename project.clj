9015e046c4f51af3d15a1482c2697f8e6519d12f
Cameron Desautels <camdez@gmail.com>
2016-04-15 21:38:22 +0800

    Upgrade to camdez/honeybadger 0.2.2

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/camdez/ring-honeybadger"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
-  :dependencies [[camdez/honeybadger "0.2.2-SNAPSHOT"]
+  :dependencies [[camdez/honeybadger "0.2.2"]
                  [org.clojure/clojure "1.7.0"]
                  [ring/ring-core "1.4.0"]])
